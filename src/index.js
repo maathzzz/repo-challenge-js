61855dc7ca700b350347a0c7e9b6aed010f38259
Hilario <juninho.gomes1234@gmail.com>
2023-02-13 18:10:38 -0300

    fix: package lock fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const user = require('./data.json')
 
-
 //calcule a diferença de tempo sendo dev entre os usuários
 //mostre o dev mais novo e o dev mais antigo (usar operadores para localizá-los)
 //mostre a diferença do de tempo de programação do daniel para seu time (vejam o forEach do JS)
